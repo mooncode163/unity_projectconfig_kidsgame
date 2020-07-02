384cdf8ac96fc8b9f0277e62af8bea566c8dea9d
chyfemail163@163.com <chyfemail163@163.com>
2020-07-02 17:32:09 +0800

    ui

--- git_upload.py
+++ git_upload.py
@@ -14,6 +14,8 @@ import socket
 def upload():
     os.system("git config --global user.email \"chyfemail163@163.com\"")
     os.system("git config --global user.name \"chyfemail163@163.com\"")
+    os.system("git config --global user.password \"Qianlizhiwai1\"")
+     
     os.system("git add .")
     os.system("git commit -m \"ui\"")
     # os.system("git commit")  
